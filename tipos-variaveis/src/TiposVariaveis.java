90cb620de231995044c1b537eb4651ef4c99717c
Fosterato <yuri.terato@hotmail.com>
2024-04-28 22:00:22 -0300

    Tipos de Variaveis Primitivas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se comecar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L;  // se comecar com zero, talvez tenha que ser outro tipo, precisa terminar com um L 
+        float pi = 3.14F; // sempre colocar um zero no final
+        double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14; // variaveis constantes 
     }
 }
