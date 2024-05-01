8f147cd12282d696cfe1a8ec4bb4a8abc284916e
Fosterato <yuri.terato@hotmail.com>
2024-05-01 18:33:03 -0300

    Condicionais

---
+++ Condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,14 @@
+public class ResultadoEscolar {
+   public static void main(String[] args) {
+    int nota = 10;
+
+    if(nota>= 7){
+        System.out.println("Aprovado");
+    }
+    else if (nota >= 5 && nota<7d){
+        System.out.println("Prova de recuperacao");
+    }
+    else
+        System.out.println("Reprovado");
+   } 
+}
